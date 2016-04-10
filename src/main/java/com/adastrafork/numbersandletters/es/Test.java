f83ba6d611769f4aacdd174c7b1ebb133ac5439c
pcesarperez <pcesarperez@gmail.com>
2016-04-11 00:30:34 +0100

    First ANTLR test
    First things first.
    
    I need to run a test using ANTLR visitor pattern to start working on
    the weights of each part of the grammar tree. To get there, I have
    changed the class `Test` to traverse the tree using the base visitor
    created by ANTLR.
    
    To run the test, we need to write:
    
    ```
    java -cp
    "dist/debug/numbers-and-letters-1.0.jar:lib/antlr-runtime-4.5.3.jar"
    com.adastrafork.numbersandletters.es.Test
    ```
    
    Not a very elegant command, but it’ll do.
    
    Apart from that, extra information has been added to the `MANIFEST.MF`
    file through the `build.properties` file included in the Gradle build
    script.

--- src/main/java/com/adastrafork/numbersandletters/es/Test.java
+++ src/main/java/com/adastrafork/numbersandletters/es/Test.java
@@ -1,12 +1,26 @@
 package com.adastrafork.numbersandletters.es;
 
-import com.adastrafork.numbersandletters.es.exceptions.UnrecognizedNumeralException;
+import com.adastrafork.numbersandletters.es.antlr4.SpanishNumeralsBaseVisitor;
+import com.adastrafork.numbersandletters.es.antlr4.SpanishNumeralsLexer;
+import com.adastrafork.numbersandletters.es.antlr4.SpanishNumeralsParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
  * Created by adastra on 30/03/16.
  */
 public class Test {
 	public static void main (String [ ] args) {
+		SpanishNumeralsLexer lexer = new SpanishNumeralsLexer (new ANTLRInputStream ("ciento noventa y nueve"));
+		SpanishNumeralsParser parser = new SpanishNumeralsParser (new CommonTokenStream (lexer));
+		SpanishNumeralsBaseVisitor<Integer> visitor = new SpanishNumeralsBaseVisitor<Integer> ( );
+		ParseTree tree = parser.numeralExpression ( );
+
+		Integer result = visitor.visit (tree);
+		System.out.println (result);
+
+		/*
 		SpanishNumeralsToNumbersConverter numeralsConverter = new SpanishNumeralsToNumbersConverter ( );
 
 		try {
@@ -14,5 +28,6 @@ public class Test {
 		} catch (UnrecognizedNumeralException e) {
 			e.printStackTrace ( );
 		}
+		*/
 	}
 }
